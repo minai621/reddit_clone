d25cecb856521143526e97f4a1b29e7a3deda571
MJ <80272444+minai621@users.noreply.github.com>
2023-01-07 06:22:58 +0000

    entity

--- server/src/entity/Entity.ts
+++ server/src/entity/Entity.ts
@@ -1,13 +1,22 @@
-import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { instanceToPlain } from 'class-transformer';
+import {
+  BaseEntity,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-// BaseEntity는 Entity의 기본 메서드들이 들어 있음.
 export default abstract class Entity extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  toJSON() {
+    return instanceToPlain(this);
+  }
 }
