1a3f94f184344c489c3bb62249a4d76aae2cedd6
MJ <80272444+minai621@users.noreply.github.com>
2023-01-07 11:18:33 +0000

    server: login

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,13 +1,18 @@
-import { validate } from "class-validator";
+import { isEmpty, validate } from "class-validator";
 import { Request, Response, Router } from "express";
 import { User } from "../entities/User";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
-const mapErrors = (errors: Object[]) => {
+
+const mapError = (errors: Object[]) => {
     return errors.reduce((prev: any, err: any) => {
-        prev[err.property] = Object.entries(err.constraints)[0][1];
-        return prev;
-    }, {})
-}
+      prev[err.property] = Object.entries(err.constraints)[0][1];
+      return prev;
+    }, {});
+  };
+  
 
 const register = async (req: Request, res: Response) => {
     const { email, username, password } = req.body;
@@ -25,8 +30,8 @@ const register = async (req: Request, res: Response) => {
         user.password = password;
 
         errors = await validate(user);
+        if (errors.length > 0) return res.status(400).json(mapError(errors));
         await user.save();
-        if (errors.length > 0) return res.status(400).json(mapErrors(errors));
         return res.json(user);
     } catch (error) {
         console.error(error);
@@ -34,7 +39,33 @@ const register = async (req: Request, res: Response) => {
     }
 }
 
+const login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+        let errors: any = {};
+        if (isEmpty(username)) errors.username = "사용자 이름은 비워둘 수 없습니다.";
+        if (isEmpty(password)) errors.password = "비밀번호는 비워둘 수 없습니다.";
+        if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+        const user = await User.findOneBy({ username });
+        if (!user) return res.status(404).json({ username: "사용자 이름이 등록되지 않았습니다." });
+        const passwordMatches = await bcrypt.compare(password, user.password);
+        if (!passwordMatches) 
+            return res.status(401).json({ password: "비밀번호가 잘못되었습니다." });
+        
+        const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+        res.set("Set-Cookie", cookie.serialize("token", token));
+        
+        return res.json({ user, token });
+    } catch (error) { 
+        console.log(error);
+        return res.status(500).json(error);
+    }
+}
+
 const router = Router();
-router.post('/register', register)
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
