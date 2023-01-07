13e53aa6e01dcda11787fb8e20f3a64eae8a2941
MJ <80272444+minai621@users.noreply.github.com>
2023-01-07 07:57:59 +0000

    server: feat cors setting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import authRoutes from './routes/auth';
 
 const app = express();
+const origin = "https://minai621-probable-happiness-w4459475xqvfxjw-3000.preview.app.github.dev";
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors({
+  origin
+}))
 
 app.get('/', (_, res) => res.send('running'));
+app.use('/api/auth', authRoutes )
 
 let port = 4000;
 
