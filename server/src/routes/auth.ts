4ec9b0a0e731d876904055d39d8677a5bc77773d
MJ <80272444+minai621@users.noreply.github.com>
2023-01-08 11:56:05 +0000

    server: login cookie

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,6 +4,8 @@ import { User } from '../entities/User';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
+import userMiddleware from '../middlewares/user';
+import authMiddleware from '../middlewares/auth';
 
 const mapError = (errors: Object[]) => {
   return errors.reduce((prev: any, err: any) => {
@@ -12,23 +14,35 @@ const mapError = (errors: Object[]) => {
   }, {});
 };
 
+const me = async (_: Request, res: Response) => {
+  return res.json(res.locals.user);
+};
+
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
+
   try {
     let errors: any = {};
+
     const emailUser = await User.findOneBy({ email });
     const usernameUser = await User.findOneBy({ username });
-    if (emailUser) errors.email = '이미 해당 이메일 주소가 사용되었습니다.';
-    if (usernameUser) errors.uername = '이미 이 사용자 이름이 사용되었습니다.';
 
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+    if (emailUser) errors.email = '이미 해당 이메일 주소가 사용되었습니다.';
+    if (usernameUser) errors.username = '이미 이 사용자 이름이 사용되었습니다.';
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
     const user = new User();
     user.email = email;
     user.username = username;
     user.password = password;
 
     errors = await validate(user);
+
     if (errors.length > 0) return res.status(400).json(mapError(errors));
+
     await user.save();
     return res.json(user);
   } catch (error) {
@@ -41,19 +55,26 @@ const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
   try {
     let errors: any = {};
+
     if (isEmpty(username))
       errors.username = '사용자 이름은 비워둘 수 없습니다.';
     if (isEmpty(password)) errors.password = '비밀번호는 비워둘 수 없습니다.';
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
 
     const user = await User.findOneBy({ username });
+
     if (!user)
       return res
         .status(404)
         .json({ username: '사용자 이름이 등록되지 않았습니다.' });
+
     const passwordMatches = await bcrypt.compare(password, user.password);
-    if (!passwordMatches)
+
+    if (!passwordMatches) {
       return res.status(401).json({ password: '비밀번호가 잘못되었습니다.' });
+    }
 
     const token = jwt.sign({ username }, process.env.JWT_SECRET);
 
@@ -68,13 +89,13 @@ const login = async (req: Request, res: Response) => {
 
     return res.json({ user, token });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.status(500).json(error);
   }
 };
 
 const router = Router();
+router.get('/me', userMiddleware, authMiddleware, me);
 router.post('/register', register);
 router.post('/login', login);
-
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,39 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
-import authRoutes from './routes/auth';
 import { AppDataSource } from './data-source';
+
+import authRoutes from './routes/auth';
+import subRoutes from './routes/subs';
+
+import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-const origin = "https://minai621-probable-happiness-w4459475xqvfxjw-3000.preview.app.github.dev";
-
+const origin =
+  'https://minai621-probable-happiness-w4459475xqvfxjw-3000.preview.app.github.dev';
+app.use(
+  cors({
+    origin,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 dotenv.config();
-app.use(cors({
-  origin,
-  credentials: true
-}))
 
 app.get('/', (_, res) => res.send('running'));
 app.use('/api/auth', authRoutes);
+app.use('/api/subs', subRoutes);
 
 let port = 4000;
-
 app.listen(port, async () => {
-  console.log(`Server running at http://localhost:${port}`);
-  AppDataSource.initialize().then(() => {
-    console.log("database initialized")
-}).catch(error => console.log(error))
+  console.log(`server running at ${process.env.APP_URL}`);
 
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('database initialized');
+    })
+    .catch((error) => console.log(error));
 });
