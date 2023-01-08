60dd79e625b4e039a9350158dcb394a8e65d5341
MJ <80272444+minai621@users.noreply.github.com>
2023-01-08 11:55:12 +0000

    client: login cookie

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,60 +3,63 @@ import React, { FormEvent, useState } from 'react';
 import { InputGroup } from '../components/InputGroup';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import { useAuthState } from '../context/auth';
 
 function Register() {
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState<any>({
-        email: '',
-        username: '',
-        password: ''
-    });
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<any>({
+    email: '',
+    username: '',
+    password: '',
+  });
 
-    let router = useRouter();
+  let router = useRouter();
 
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault();
-        try {
-            const res = await axios.post('/auth/register', {
-                email,
-                username,
-                password
-            })
-            console.log(res);
-            router.push('/login');
-        } catch (error: any) {
-            console.log(error);
-            setErrors(error.response?.data || {})
-        }
-    
-}
+  const { authenticated } = useAuthState();
+  if (authenticated) router.push('/');
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post('/auth/register', {
+        email,
+        username,
+        password,
+      });
+      console.log(res);
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error);
+      setErrors(error.response?.data || {});
+    }
+  };
 
   return (
     <div className='bg-white'>
       <div className='flex flex-col items-center justify-center h-screen p-6'>
         <div className='w-10/12 mx-auto md:w-96'>
           <h1 className='mb-2 text-lg font-medium'>회원가입</h1>
-                  <form onSubmit={handleSubmit}>
-                      <InputGroup
-                          placeholder='Email'
-                          value={email}
-                          setValue={setEmail}
-                          error={errors.email}
-                      />
-                      <InputGroup
-                          placeholder='username'
-                          value={username}
-                          setValue={setUsername}
-                          error={errors.username}
-                      />
-                      <InputGroup
-                          placeholder='Password'
-                          value={password}
-                          setValue={setPassword}
-                          error={errors.password}
-                      />
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder='Email'
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder='username'
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder='Password'
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
             <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
               회원가입
             </button>
@@ -64,7 +67,7 @@ function Register() {
           <small>
             이미 가입하셨나요?
             <Link href='/login' className='ml-1 text-blue-500 uppercase'>
-                로그인
+              로그인
             </Link>
           </small>
         </div>
