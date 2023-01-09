532d48c975b08afddcac8972e9b4c7c05e54df07
MJ <80272444+minai621@users.noreply.github.com>
2023-01-09 10:47:15 +0000

    server: error fix

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -12,29 +12,35 @@ const router = Router();
 
 const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
+
   try {
     let errors: any = {};
-    if (isEmpty(name)) errors.name = '이름을 비워둘 수 없습니다.';
-    if (isEmpty(title)) errors.title = '제목을 비워둘 수 없습니다.';
+    if (isEmpty(name)) errors.name = '이름은 비워둘 수 없습니다.';
+    if (isEmpty(title)) errors.title = '제목은 비워두 수 없습니다.';
 
     const sub = await AppDataSource.getRepository(Sub)
       .createQueryBuilder('sub')
       .where('lower(sub.name) = :name', { name: name.toLowerCase() })
       .getOne();
+
     if (sub) errors.name = '서브가 이미 존재합니다.';
     if (Object.keys(errors).length > 0) {
       throw errors;
     }
   } catch (error) {
-    return res.status(500).json(error);
+    console.log(error);
+    return res.status(500).json({ error: '문제가 발생했습니다.' });
   }
+
   try {
     const user: User = res.locals.user;
 
     const sub = new Sub();
     sub.name = name;
-    sub.title = title;
     sub.description = description;
+    sub.title = title;
+    sub.user = user;
+
     await sub.save();
     return res.json(sub);
   } catch (error) {
